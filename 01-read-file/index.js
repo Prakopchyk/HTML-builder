b5ed60225696129d2fea034a0f5913f01e5dc515
Prakopchyk <prokopchike1987@gmail.com>
2023-05-05 21:35:38 +0200

    feat: implement 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
+let filePath = path.join(__dirname, 'text.txt');
+const readableStream = fs.createReadStream(filePath, 'utf-8');
+
+readableStream.on('data', data => console.log(data));
+readableStream.on('error', error => console.log('Error', error.message));
