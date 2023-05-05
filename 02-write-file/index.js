140f18aa089567b5528e211d16466f82b5346a8e
Prakopchyk <prokopchike1987@gmail.com>
2023-05-06 00:30:16 +0200

    feat: implement 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+let filePath = path.join(__dirname, 'text.txt');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log('Здравствуй!');
+console.log('Как дела?');
+
+process.on('exit', (code) => {
+  console.log('Засим прощаюсь. Успехов!');
+});
+
+rl
+  .on('line', function (line) {
+    if (line === "exit") {
+      process.exit();
+    }
+
+    fs.appendFileSync(filePath, `${line}\n`)
+    console.log('Как дела?')
+
+  });
